bb8fad3783f9adace253f2c42959b09184c3074d
Nimrod Bahar <nimrodbahar@gmail.com>
2016-05-13 11:44:54 +0200

    seeding testimonials

--- app/assets/javascripts/startchange.js
+++ app/assets/javascripts/startchange.js
@@ -1,6 +1,6 @@
 var a = $(".navbar-wagon-fixed").offset().top;
 var b = $(".navbar-wagon-link").offset().top;
-var c = $(".navbar-wagon-item").offset().top;
+// var c = $(".navbar-wagon-item").offset().top;
 
 
 
@@ -17,11 +17,5 @@ $(document).scroll(function(){
        $('.navbar-wagon-link').css({"color":"#494949"});
     } else {
        $('.navbar-wagon-link').css({"color":"white"});
-    }
-    if($(this).scrollTop() > b)
-    {
-       $('.navbar-wagon-item').css({"color":"#494949"});
-    } else {
-       $('.navbar-wagon-item').css({"color":"white"});
     }
 });
